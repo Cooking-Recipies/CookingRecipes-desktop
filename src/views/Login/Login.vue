4cc8882c36683d20b79066eb94a5a9ebfaa45059
krysduda <krystianduda64@gmail.com>
2021-03-26 14:34:39 +0100

    Updated package lock

---
+++ src/views/Login/Login.vue
@@ -0,0 +1,21 @@
+<template>
+    <b-form>
+        <b-form-group label-cols="4" label-cols-lg="2" label-size="lg" label="Username" label-for="username">
+            <b-form-input size="lg" placeholder="login" id="username" required></b-form-input>
+        </b-form-group>
+        <b-form-group label-cols="4" label-cols-lg="2" label-size="lg" label="Password" label-for="password">
+            <b-form-input size="lg" placeholder="password" id="password" required></b-form-input>
+        </b-form-group>
+        <b-button size="lg" type="submit">Login</b-button>
+    </b-form>
+</template>
+
+<script>
+    export default {
+        name: "Login"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Login/Register.vue
@@ -0,0 +1,25 @@
+<template>
+    <b-form>
+        <b-form-group label-cols="4" label-cols-lg="2" label-size="md" label="Username" label-for="username">
+            <b-form-input size="md" id="username" required></b-form-input>
+        </b-form-group>
+        <b-form-group label-cols="4" label-cols-lg="2" label-size="md" label="Password" label-for="password">
+            <b-form-input size="md" id="password" required></b-form-input>
+        </b-form-group>
+        <b-form-group label-cols="4" label-cols-lg="2" label-size="md" label="Password confirmation"
+                      label-for="password_conf">
+            <b-form-input size="md" id="password_conf" required></b-form-input>
+        </b-form-group>
+        <b-button size="lg" type="submit">Register</b-button>
+    </b-form>
+</template>
+
+<script>
+    export default {
+        name: "Register"
+    }
+</script>
+
+<style scoped>
+
+</style>
