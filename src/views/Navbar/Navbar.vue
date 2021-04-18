3d58a1d7eb1191dd30997c1a3f30593ed016e09a
ColdShot <krystianduda64@gmail.com>
2021-04-18 20:43:18 +0200

    Navbar updates when user logs in

--- src/views/Navbar/Navbar.vue
+++ src/views/Navbar/Navbar.vue
@@ -1,41 +1,60 @@
 <template>
-    <div>
-        <b-navbar toggleable="lg" type="dark" variant="info">
-            <b-navbar-brand to="/">MAIN</b-navbar-brand>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand to="/">MAIN</b-navbar-brand>
 
-            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-            <b-collapse id="nav-collapse" is-nav>
-                <b-navbar-nav>
-                    <b-nav-item to="/">Page 1</b-nav-item>
-                    <b-nav-item to="/">Page 2</b-nav-item>
-                    <b-nav-item to="/">Page 3</b-nav-item>
-                    <b-nav-item to="about">About</b-nav-item>
-                </b-navbar-nav>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item to="/">Page 1</b-nav-item>
+          <b-nav-item to="/">Page 2</b-nav-item>
+          <b-nav-item to="/">Page 3</b-nav-item>
+          <b-nav-item to="about">About</b-nav-item>
+        </b-navbar-nav>
 
-                <b-navbar-nav class="ml-auto">
-                    <b-nav-form>
-                        <b-form-input size="sm" class="mr-sm-2" placeholder="Search for a recipe"></b-form-input>
-                        <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-                    </b-nav-form>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-form>
+            <b-form-input size="sm" class="mr-sm-2" placeholder="Search for a recipe"></b-form-input>
+            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+          </b-nav-form>
 
-                    <b-nav-item-dropdown right>
-                        <template #button-content>
-                            <em>User</em>
-                        </template>
-                        <b-dropdown-item to="/login">Sign in</b-dropdown-item>
-                        <b-dropdown-item to="/register">Register</b-dropdown-item>
-                    </b-nav-item-dropdown>
-                </b-navbar-nav>
-            </b-collapse>
-        </b-navbar>
-    </div>
+          <b-nav-item-dropdown right>
+            <template #button-content>
+              <b-img src="@/assets/user.svg" alt="Account"></b-img>
+            </template>
+            <div v-if="!isLoggedIn">
+              <b-dropdown-item to="/login">Sign in</b-dropdown-item>
+              <b-dropdown-item to="/register">Register</b-dropdown-item>
+            </div>
+            <div v-if="isLoggedIn">
+              <b-dropdown-item to="/profile">Manage account</b-dropdown-item>
+              <b-dropdown-item to="/logout">Logout</b-dropdown-item>
+            </div>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Navbar"
+export default {
+  name: "Navbar",
+  data: function() {
+    return {
+      isLoggedIn: false,
     }
+  },
+  created() {
+    let user = localStorage.getItem("username")
+    if (user !== null) {
+      this.isLoggedIn = true
+    }
+    console.log("created")
+  }
+
+}
 </script>
 
 <style scoped>
