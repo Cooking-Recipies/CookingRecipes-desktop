df73bd9550fb3a6f12100aa1121d88b946bd609b
krysduda <krystianduda64@gmail.com>
2021-04-20 13:50:54 +0200

    Finished draft of AddRecipe component

--- src/views/Recipes/AddRecipe.vue
+++ src/views/Recipes/AddRecipe.vue
@@ -1,8 +1,36 @@
 <template>
   <b-row align-v="start">
     <b-col>
-      <input type="file" id="photos" ref="photos" multiple v-on:change="handlePhotos"/>
-      <b-button @click="submitPhotos"></b-button>
+
+      <b-form id="container">
+        <b-row align-content="center">
+          <b-col>
+            <h1>Select photos to upload</h1>{{"\n"}}
+            <input type="file" id="photos" ref="photos" multiple v-on:change="handlePhotos"/>
+          </b-col>
+        </b-row>
+
+        <b-row>
+          <b-col>
+            <h1>Ingredients</h1>
+            <b-textarea></b-textarea>
+          </b-col>
+        </b-row>
+
+        <b-row>
+          <b-col>
+            <h1>Steps</h1>
+            <b-textarea></b-textarea>
+          </b-col>
+        </b-row>
+
+        <b-row>
+          <b-col>
+            <b-button @click="submitPhotos">Upload recipe</b-button>
+          </b-col>
+        </b-row>
+      </b-form>
+
     </b-col>
   </b-row>
 </template>
@@ -10,19 +38,19 @@
 <script>
 export default {
   name: "AddRecipe",
-  data: function () {
-    return {
-      correctPhotos: [],
-      approvedExtensions: ["image/png", "image/svg+xml", "image/jpeg"]
-    }
-  },
-  methods: {
+    data: function () {
+      return {
+        correctPhotos: [],
+        approvedMimeExtensions: ["image/png", "image/svg+xml", "image/jpeg"]
+      }
+    },
+    methods: {
+      submitPhotos: function () {
+        console.log(this.correctPhotos)
+    },
     handlePhotos: function () {
       let userPhotos = this.$refs.photos.files
       this.addCorrectPhotos(userPhotos)
-    },
-    submitPhotos: function () {
-      console.log(this.correctPhotos)
     },
     addCorrectPhotos: function (userPhotos) {
       for (let i = 0; i < userPhotos.length; i++) {
@@ -34,12 +62,14 @@ export default {
       }
     },
     isExtensionValid: function (mimeExtension) {
-      return this.approvedExtensions.includes(mimeExtension);
+      return this.approvedMimeExtensions.includes(mimeExtension)
     }
   }
 }
 </script>
 
 <style scoped>
-
+#container div {
+  padding: 10px;
+}
 </style>
